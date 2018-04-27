1e0f93992b791b64aec6fcef88b702ae29c1a68b
serp-ya <sergei.kundryukov1989@gmail.com>
2018-04-28 00:41:27 +0300

    Изменена логика в соответствие с замечаниями

--- VkWorker.py
+++ VkWorker.py
@@ -6,8 +6,8 @@ import time
 class VkWorker:
     api_url = 'https://api.vk.com/method'
     params = {
-        'access_token': '7b23e40ad10e08d3b7a8ec0956f2c57910c455e886b' \
-            '480b7d9fb59859870658c4a0b8fdc4dd494db19099',
+        'access_token': '1fba8861513553efb45c7951a97ddeffb3b5534097126ca538b93' \
+                        'fd5f32d80818ac9b2a1972b5971c0e41',
         'v': '5.74',
     }
 
@@ -31,56 +31,32 @@ class VkWorker:
         return f'{self.api_url}/{method_name}'
 
 
-    def make_get_request(self, method_name):
+    def make_get_request(self, method_name, user_id=None):
         method_url = self.formatted_url(method_name)
 
         params = self.params.copy()
-        params['user_id'] = self.user_id
+        params['user_id'] = user_id or self.user_id
 
-        if method_name == 'groups.get':
-            params['fields'] = 'members_count'
-            params['extended'] = 1
+        result = requests.get(method_url, params=params).json()
+        time.sleep(0.35)
 
-        return requests.get(method_url, params=params).json()
+        return result
 
 
-    def get_friends(self):
+    def get_friends(self,):
         return self.make_get_request('friends.get')
 
 
-    def get_groups(self):
-        return self.make_get_request('groups.get')
+    def get_groups(self, user_id=None):
+        return self.make_get_request('groups.get', user_id)
 
-
-    def detect_friend(self, group_id, user_ids):
-        method_url = self.formatted_url('groups.isMember')
+    def get_groups_info(self, group_ids):
+        method_url = self.formatted_url('groups.getById')
         params = self.params.copy()
-        params['group_id'] = group_id
-        per_request_ids_count = 250
-        result = False
+        params['fields'] = 'members_count'
+        params['group_ids'] = ','.join(map(str, group_ids))
 
-        iterations = math.ceil(len(user_ids) / per_request_ids_count)
-
-        for i in range(iterations):
-            count_start = i * per_request_ids_count
-            count_finish = (i + 1) * per_request_ids_count
-            params_of_part = params.copy()
-            user_ids_part = user_ids[count_start:count_finish]
-
-            params_of_part['user_ids'] = ','.join(map(str, user_ids_part))
-
-            detect_response = requests.get(method_url, params=params_of_part).json()
-            time.sleep(0.35)
-
-            detect__users = detect_response['response']
-
-            friends_in_group = [*filter(
-                lambda item: item['member'] == 1,
-                detect__users
-            )]
-
-            if len(friends_in_group) > 0:
-                result = True
-                break
+        result = requests.get(method_url, params=params).json()
+        time.sleep(0.35)
 
         return result
--- main.py
+++ main.py
@@ -23,53 +23,50 @@ def core():
     if entered_value.lower() == 'c':
         return
 
+    if entered_value and len(entered_value) > 1:
+        vk_worker = VkWorker(entered_value)
+
+        friends_response = vk_worker.get_friends()
+        friends_list = friends_response['response']['items']
+
+        victim_groups_response = vk_worker.get_groups()
+        victim_groups_list = set(victim_groups_response['response']['items'])
+
+        friends_groups = set()
+
+        print('Идёт поиск групп...')
+
+        for i, friend_id in enumerate(friends_list):
+            print(f'Прогресс {to_fixed(i / len(friends_list) * 100)}%')
+
+            resp = vk_worker.get_groups(friend_id)
+
+            try:
+                friend_groups = resp['response']['items']
+                friends_groups.update(friend_groups)
+            except:
+                'do nothing'
+
+        print('Подводим итоги...')
+        intersection_groups = set.intersection(victim_groups_list, friends_groups)
+        unique_groups = set.difference(victim_groups_list, intersection_groups)
+        result = list()
+
+        if len(unique_groups):
+            unique_groups_full_info = vk_worker.get_groups_info(unique_groups)['response']
+
+            for group in unique_groups_full_info:
+                formated_group_info = group_object_factory(group)
+                result.append(formated_group_info)
+
+        with open(result_file_name, 'w', encoding='utf8') as res_file:
+            res_file.write(json.dumps(result, ensure_ascii=False))
+
+            print(f'Результат записан в файл {result_file_name}\n')
 
 
-    try:
-        if entered_value and len(entered_value) > 1:
-            vk_worker = VkWorker(entered_value)
-
-            friends_response = vk_worker.get_friends()
-            friends_list = friends_response['response']['items']
-
-            groups_response = vk_worker.get_groups()
-            groups_list = groups_response['response']['items']
-
-            # Для демонстрации
-            # START
-            print('\n############### Для демонстрации')
-            friends_count = friends_response['response']['count']
-            groups_count = groups_response['response']['count']
-            print('friends_count', friends_count)
-            print('groups_count', groups_count)
-            print('############### Для демонстрации\n')
-            # END
-            # Для демонстрации
-
-            result = list()
-
-            for i, group in enumerate(groups_list):
-                group_id = group['id']
-
-                print(f'Прогресс {to_fixed(i / len(groups_list) * 100)}%')
-                print(f'Группа {group_id} на проверке')
-                has_friends = vk_worker.detect_friend(group_id, friends_list)
-
-                if not has_friends:
-                    group_object = group_object_factory(group)
-                    result.append(group_object)
-
-            with open(result_file_name, 'w', encoding='utf-8') as res_file:
-                print(json.dumps(result), file=res_file)
-                print(f'Результат записан в файл {result_file_name}\n')
-
-
-        else:
-            print('Введены не корректные данные')
-
-
-    except:
-        print('Возникла ошибка, попробуйте ещё раз')
+    else:
+        print('Введены не корректные данные')
 
 
     return core()
